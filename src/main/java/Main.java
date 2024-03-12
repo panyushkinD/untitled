b2b384a55b1217ed6faec237dd80b9b6da1d6e4a
Панюшкин Данила Дмитриевич <panyushkin@sweetlife.ru>
2024-03-12 22:32:41 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int firstName = scanner.nextInt();
+            int lastName = scanner.nextInt();
+            char c = scanner.next().charAt(0);
+            for (int i = 0; i < 50; i++) {
+                System.out.println();
+
+            }
+
+
+            if (c == '+') {
+
+                int rez = firstName + lastName;
+                System.out.println(rez);
+
+            } else if (c == '-') {
+                int rez = firstName - lastName;
+                System.out.println(rez);
+
+            } else {
+                System.out.println("Не ");
+            }
+        } catch (Exception e){
+            System.out.println("Ошибка");
+        }
+    }
+}
